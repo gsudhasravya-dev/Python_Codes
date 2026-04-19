bd1b60c97b03c8855415a9ca60988cfc42e9d6a2
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-04-19 23:36:58 +0530

    ..prog

---
+++ prog49.py
@@ -0,0 +1,10 @@
+eng=float(input("enter marks:"))
+math=float(input("enter marks:"))
+sci=float(input("enter marks:"))
+soc=float(input("enter marks:"))
+total=eng+math+sci+soc
+avg=total/4
+percentage=(total/400)*100
+print(total)
+print(avg)
+print(percentage)
