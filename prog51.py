60f3131d0fef43a3b03d290f33687b95ee377756
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-04-19 23:41:27 +0530

    ..prog

---
+++ prog51.py
@@ -0,0 +1,5 @@
+pri_amt=float(input("enter principal amount:"))
+t=float(input("enter time in months:"))
+rate=float(input("enter rate of interest:"))
+simple_interest=(pri_amt*t*rate)/100
+print(simple_interest)
