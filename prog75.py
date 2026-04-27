5f660a84fd1da7c7cabd7a939ed5b5be49a26794
gsudhasravya-dev <gsudhasravya@gmail.com>
2026-04-27 23:14:18 +0530

    added prog file

---
+++ prog75.py
@@ -0,0 +1,8 @@
+num1=int(input("enter a number:"))
+num2=int(input("enter a number:"))
+if(num1==num2):
+    print("equal")
+elif(num1<num2):
+    print("num2 greater")
+else:
+    print("num1 greater")
